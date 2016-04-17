eb000c90547b5efc272c850d254b20bc9af1c52a
David Livingstone <david@david.tl>
2016-04-17 11:16:08 -0700

    completed all TODOs

--- class-05-new-article-creation/lecture/starter-code/scripts/articleView.js
+++ class-05-new-article-creation/lecture/starter-code/scripts/articleView.js
@@ -65,6 +65,7 @@ articleView.setTeasers = function() {
 articleView.initNewArticlePage = function() {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
   $('.tab-content').show();
+
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we
   // have data to export. Also, let's add focus event to help us select the JSON.
@@ -72,6 +73,7 @@ articleView.initNewArticlePage = function() {
   $('#article-json').on('focus', function() {
     $(this).select();
   });
+
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
   $('#new-form').on('change', 'input', 'textarea', articleView.create);
 };
