fad3ebbe9bd20d36680797180fe4c1c00d9cbdaa
Chris Perez <chris.k.perez@gmail.com>
2016-04-25 13:22:34 -0700

    Added admin page to the SPA and added a link to the admin page on the bang in the header h1

---
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/adminController.js
@@ -0,0 +1,11 @@
+(function(module) {
+  var adminController = {};
+
+  adminController.index = function() {
+    Article.fetchAll(articleView.initAdminPage);
+    $('main > section').hide();
+    $('#blog-stats').show();
+  };
+
+  module.adminController = adminController;
+})(window);
